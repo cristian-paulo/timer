faa2419bf1cee1e30691eb35a864b231921de6f7
Cristian Paulo <cristian_paulo@hotmail.com>
2015-08-27 14:23:22 -0300

    doing function setInterval

--- js/js.js
+++ js/js.js
@@ -2,9 +2,25 @@
 
 window.onload = function () 
 {
+	
+
+	var hour = 1, minute = 60, seconds = 9;
 	document.getElementById("start").onclick = function()
 	{
-		var local = document.getElementsByClassName("center")[0];
-		//alert(local.innerHTML);
-	}
+		var local = document.getElementsByClassName("center")[0];			
+		
+		var inter = setInterval(function()
+		{
+			local.innerHTML = hour + ":" + minute + ":" + seconds;
+			seconds--;
+
+			if (seconds < 0)
+			{
+				clearInterval(inter);
+		alert(seconds);
+			}
+		}, 1000);
+
+
+	}		
 }
