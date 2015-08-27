322f9b36059f399555ff078bd5c92addd067dac8
Cristian Paulo <cristian_paulo@hotmail.com>
2015-08-27 13:16:35 -0300

    create js

---
+++ js/js.js
@@ -0,0 +1,10 @@
+
+
+window.onload = function () 
+{
+	document.getElementById("start").onclick = function()
+	{
+		var local = document.getElementsByClassName("center")[0];
+		//alert(local.innerHTML);
+	}
+}
